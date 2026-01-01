cf4975be16c1202719e0eed21e18965fa3a73563
Shamina Jasmine <janee2janee8090@gmail.com>
2026-01-01 19:39:09 +0530

    hum

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Product from "./Components/Product";
-import HeroTestimonials from "./Components/Testmonials";
+
 
 
 function App() {
   return (
     <div>
-     <HeroTestimonials/>
-     <Product/>
+   
     </div>
   );
 }
